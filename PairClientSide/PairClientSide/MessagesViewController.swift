7f16e59b835c19f6e8b77b538f752eff8ad8431e
William Huang <whuang6@stanford.edu>
2015-01-05 21:49:59 -0800

    progress on the title page

---
+++ PairClientSide/PairClientSide/MessagesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MessagesViewController.swift
+//  PairClientSide
+//
+//  Created by William Huang on 1/5/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import Foundation
--- PairClientSide/PairClientSide/ViewController.swift
+++ PairClientSide/PairClientSide/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pairTitle: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pairTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        pairTitle.textAlignment = NSTextAlignment.Center
+        pairTitle.center = CGPointMake(UIScreen.mainScreen().bounds.width / 2.0, UIScreen.mainScreen().bounds.height / 4.0)
+        pairTitle.text = "Pair"
+        pairTitle.font = UIFont(name: "Helvetica Light", size: 60.0)
+        self.view.addSubview(pairTitle)
     }
 
     override func didReceiveMemoryWarning() {
