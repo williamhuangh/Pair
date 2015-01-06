8284cac0e6ce4874aacabc577211fcbedbc0a363
William Huang <whuang6@stanford.edu>
2015-01-05 20:40:37 -0800

    initial client side commit

---
+++ PairClientSide/PairClientSide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PairClientSide
+//
+//  Created by William Huang on 1/5/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
